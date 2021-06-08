6c5a40b08a6a0f97433040c810df4ed22c9bd256
LUNDI19 <michaelmcloughlin9@gmail.com>
2021-06-08 15:48:23 +0200

    Our cat webapp

--- app.rb
+++ app.rb
@@ -1,10 +1,16 @@
 require 'sinatra'
 require "sinatra/reloader" if development?
 
-get '/' do
-  "Hello World"
-end
+# get '/' do
+#   "Hello World"
+# end
 
-get '/secret' do
-  "Message to mike"
+# get '/secret' do
+#   "Message to mike"
+# end
+
+get '/cat' do
+  "<div style='border: 3px dashed red'>
+  <img src='https://i.imgur.com/jFaSxym.png'>
+ </div>"
 end
